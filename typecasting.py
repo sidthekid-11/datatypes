ddca75bd1be56c034bf35325d4d71f1d1b32dfe9
sidthekid-11 <rakheesiddhant2013@gmail.com>
2025-11-03 16:57:57 +0530

    initial commit

---
+++ typecasting.py
@@ -0,0 +1,21 @@
+name ="siddhant"
+age = 12
+is_student=True
+weight=39.5
+
+print("datatype of name:",type(name))
+
+print("datatype of age:",type(age))
+
+print("datatype of is_student:",type(is_student))
+
+print("datatype of weight:",type(weight))
+
+print("\nafter typecasting")
+
+age = str(age)
+print("datatype of age:",type(age))
+
+weight = int(weight)
+print("datatype of weight:",type(weight))
+
